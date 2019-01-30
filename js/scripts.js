5673b5eac75b65a20f7e9b6c343d9c83d4a107e3
Billy Shelton <sheltonbilly@utexas.edu>
2019-01-29 21:27:05 -0600

    Made the webpage work properly, refactored code and added comments
    When an employee's gallery card is clicked, their modal card is displayed on the screen.  When the modal window x button is clicked, the modal window is closed.  The process is repeatable with any other card clicks.  Also, the number of loops for the for loops determines how much data is retrieved from the server.

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,85 @@
+// The following file is a Javascript file which manages pulls employee
+// data from an API and appends it to the corresponding index.html file
 
-//this for loop creates 12 XMLHttp Requests to call the user data.  
+//An array to hold the employee modal cards is created
+let employeeModalCards = [];
+
+//An array to hold the employee gallery cards is created
+let employeeGalleryCards = [];
+
+//This function creates an HTML element with the employee gallery card data and appends it to the DOM
+function showEmployeeGalleryCard(employee, employeeGalleryCards, i){
+	let galleryHTML = '<div class="card-img-container">';
+	galleryHTML += '<img class="card-img" src="'+ employee.results[0].picture.large + '" alt="profile picture">';
+	galleryHTML += '</div>';
+	galleryHTML += '<div class="card-info-container">';
+	galleryHTML += '<h3 id="name" class="card-name cap">' + employee.results[0].name.first + ' '+ employee.results[0].name.last + '</h3>';
+	galleryHTML += '<p class="card-text">' + employee.results[0].email + '</p>';
+	galleryHTML += '<p class="card-text cap">' + employee.results[0].location.city +', ' + employee.results[0].location.state + '</p>';
+	galleryHTML += '</div>';
+	let userHTML = document.createElement('div');
+	userHTML.innerHTML = galleryHTML;
+	userHTML.className = 'card';
+	employeeGalleryCards[i] = userHTML;
+	document.getElementById('gallery').appendChild(userHTML);
+}
+
+//This function creates an HTML element with the employee modal card data
+function createEmployeeModalCard(employee, employeeModalCards, i){
+	let userModalHTML = document.createElement('div');
+	userModalHTML.className = 'modal-container';
+	let modalHTML = '<div class="modal">';
+	modalHTML += '<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>';
+	modalHTML += '<div class="modal-info-container">';
+	modalHTML += '<img class="modal-img" src="' + employee.results[0].picture.large + '" alt="profile picture">';
+	modalHTML += '<h3 id="name" class="modal-name cap">' + employee.results[0].name.first + ' ' + employee.results[0].name.last + '</h3>';
+	modalHTML += '<p class="modal-text">' + employee.results[0].email + '</p>';
+	modalHTML += '<p class="modal-text cap">' + employee.results[0].location.city + '</p>';
+	modalHTML += '<hr>';
+	modalHTML += '<p class="modal-text">' + employee.results[0].phone + '</p>';
+	modalHTML += '<p class="modal-text">' + employee.results[0].location.street + ', ' + employee.results[0].location.city + ', ' + employee.results[0].location.state + ', ' + employee.results[0].location.postcode + '</p>'; 
+	modalHTML += '<p class="modal-text">Birthday: '; + employee.results[0].dob.date.slice(5,7) + '/' + employee.results[0].dob.date.slice(8,10) + '/' + employee.results[0].dob.date.slice(2,4) + '</p>';
+	modalHTML += '</div>';
+	modalHTML += '</div>';
+	userModalHTML.innerHTML = modalHTML;
+	//The employee modal card is added to the employee modal array
+	employeeModalCards[i] = userModalHTML;
+}
+
+//The following for loop determines how many employees to display on the page 
 for(let i = 0; i < 12; i++) {	
-	//an XMLHttpRequest object is created
+	//An XMLHttpRequest object is created to retrieve an employee's info
 	var galleryXHR = new XMLHttpRequest();
 
-	//USER GALLERY CARD
-	//when the response is ready, the user image, name, location and email and appended to the DOM with this callback function
+	//When the response is ready, the employee image, name, location and email are appended to the DOM with this callback function
 	galleryXHR.onreadystatechange = function() {
 		if(galleryXHR.readyState === XMLHttpRequest.DONE) {
-			//the JSON returned from the server is parsed
+			//The JSON returned from the server is parsed
 			let employee = JSON.parse(this.responseText);
-			//HTML is created for the user info to be appended to gallery ID
-			let galleryHTML = '<div class="card-img-container">';
-			galleryHTML += '<img class="card-img" src="'+ employee.results[0].picture.large + '" alt="profile picture">';
-			galleryHTML += '</div>';
-			galleryHTML += '<div class="card-info-container">';
-			galleryHTML += '<h3 id="name" class="card-name cap">' + employee.results[0].name.first + ' '+ employee.results[0].name.last + '</h3>';
-			galleryHTML += '<p class="card-text">' + employee.results[0].email + '</p>';
-			galleryHTML += '<p class="card-text cap">' + employee.results[0].location.city +', ' + employee.results[0].location.state + '</p>';
-			galleryHTML += '</div>';
-			let userHTML = document.createElement('div');
-			userHTML.innerHTML = galleryHTML;
-			userHTML.className = 'card';
-			document.getElementById('gallery').appendChild(userHTML);
 
-			//HTML is created for the user modal
-			let modalHTML = '<div class="modal">';
-			modalHTML += '<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>';
-			modalHTML += '<div class="modal-info-container">';
-			modalHTML += '<img class="modal-img" src="' + employee.results[0].picture.large + '" alt="profile picture">';
-			modalHTML += '<h3 id="name" class="modal-name cap">' + employee.results[0].name.first + ' ' + employee.results[0].name.last + '</h3>';
-			modalHTML += '<p class="modal-text">' + employee.results[0].email + '</p>';
-			modalHTML += '<p class="modal-text cap">' + employee.results[0].location.city + '</p>';
-			modalHTML += '<hr>';
-			modalHTML += '<p class="modal-text">' + employee.results[0].phone + '</p>';
-			modalHTML += '<p class="modal-text">' + employee.results[0].location.street + ', ' + employee.results[0].location.city + ', ' + employee.results[0].location.state + ', ' + employee.results[0].location.postcode + '</p>'; 
-			modalHTML += '<p class="modal-text">Birthday: '; + employee.results[0].dob.date.slice(5,7) + '/' + employee.results[0].dob.date.slice(8,10) + '/' + employee.results[0].dob.date.slice(2,4) + '</p>';
-			modalHTML += '</div>';
-			modalHTML += '</div>';
-			//console.log(employee.results[0]);
-			let userModalHTML = document.createElement('div');
-			document.getElementsByTagName('BODY')[0].appendChild(userModalHTML);
+			//The employee gallery card is created, added to the employee gallery array and appended to the DOM
+			showEmployeeGalleryCard(employee, employeeGalleryCards, i);
+			
+			//The employee modal card is created and added to the employee modal array
+			createEmployeeModalCard(employee, employeeModalCards, i);
+			
+			//The employee modal card is opened when it is clicked
+			employeeGalleryCards[i].addEventListener('click',()=>{
+				let currentEmployeeModal = employeeModalCards[i];
+				document.getElementsByTagName('BODY')[0].appendChild(currentEmployeeModal);
 
-			//FIXXXXXXXXX
-			//the user card is opened when it is clicked
-			userHTML.addEventListener('click',()=>{
-				userModalHTML.style.display = 'inline-block';
-				userModalHTML.className = 'modal-container';
-				userModalHTML.innerHTML = modalHTML;
-
-				//FIXXXXXXXXXXX
-				//when the modal x button is clicked the modal is hidden from display
+				//When the modal X button is clicked the employee modal is hidden from display
 				document.getElementById('modal-close-btn').addEventListener('click', ()=>{
-					userModalHTML.style.display = 'none';
-					//userModalHTML.parentNode.removeChild(userModalHTML);
+					currentEmployeeModal.parentNode.removeChild(currentEmployeeModal);
 				});
 			});
 
 		} 
 	};
 
-	//a request is opened
+	//A request is opened for the current employee info
 	galleryXHR.open('GET', 'https://randomuser.me/api/?format=json', false);
-	//the request is sent to the server
+
+	//The request is sent to the server
 	galleryXHR.send();
 }
